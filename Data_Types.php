02fa9f3f5540cf81565df90249d8a43c1517f4b3
IstJunior <istjuniorx@gmail.com>
2022-07-12 21:28:26 -0500

    Add boolean to DataTypes

--- Data_Types.php
+++ Data_Types.php
@@ -28,7 +28,20 @@ var_dump($num2);
 echo "<hr/>";
 
 //ARRAY
+$sports = array("Soccer","Tennis","Baseball","Football");
+var_dump($sports);
+echo "<hr/>";
 
+//NULL
+$color = "Blue";
+$color = null;
+var_dump($color);
+
+//BOOLEAN
+$x = true;
+$y = false;
+var_dump($x);
+var_dump($y);
 
     ?>
 </body>
